b37fe243ca27dfdd82538ef92019ffd0e5f45f5d
Ghevond97 <ghevond.mirzakhanyan@gmail.com>
2019-08-04 00:27:34 +0400

    additional dependencies for makaing api calls, and redux action creation

---
+++ src/Components/TablesContainer/TablesContainer.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class TablesContainer extends Component {
+  componentDidMount() {
+    
+  }
+
+  render() {
+    return(
+      <div>
+        Container
+      </div>
+    )
+  }
+}
+
+export default TablesContainer;
